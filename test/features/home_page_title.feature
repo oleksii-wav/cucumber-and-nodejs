a1b5868e149cff677debad9df53cc88218518610
Oleksii Fedorenko <oleksii.fedorenko.94@gmail.com>
2022-11-01 12:35:22 +0400

    Add parameters to step definitions

--- test/features/home_page_title.feature
+++ test/features/home_page_title.feature
@@ -1,7 +1,12 @@
+@smoke
 Feature: Home Page
 
-@smoke
 Scenario: Check Title of Home Page
-  Given I open epam.com
-  Then Page title should be 'EPAM | Enterprise Software Development, Design & Consulting'
-  When I wait 10 seconds
+  Given I open "https://www.epam.com" url
+  Then Page title should be "EPAM | Enterprise Software Development, Design & Consulting"
+  When I wait "3" seconds
+
+Scenario: Check Title of Home Page
+  Given I open "http://bashorg.org/" url
+  Then Page title should be "Bashorg.org - Лучший Цитатник Рунета - Башорг.орг"
+  When I wait "3" seconds
--- test/step_definitions/browser_steps.js
+++ test/step_definitions/browser_steps.js
@@ -3,15 +3,15 @@ const { expect } = require('chai');
 
 setDefaultTimeout(60000);
 
-When(/^I open epam.com$/, () => {
-  return browser.get('https://www.epam.com');
+When('I open {string} url', (url) => {
+  return browser.get(url);
 });
 
-Then(/^Page title should be 'EPAM | Enterprise Software Development, Design & Consulting'$/, async () => {
+Then(/^Page title should be "([^"]*)"$/, async (title) => {
   const pageTitle = await browser.getTitle();
-  expect(pageTitle).to.be.equal('EPAM | Enterprise Software Development, Design & Consulting');
+  expect(pageTitle).to.be.equal(title);
 });
 
-When(/^I wait 10 seconds$/, () => {
-  return browser.sleep(10000)
+When('I wait "{int}" seconds', (timeInSeconds) => {
+  return browser.sleep(timeInSeconds * 1000)
 });
